25d4088a7071110a957f78575c76adba95d727c3
Shawn Tucker <shawnxman1423@gmail.com>
2024-11-17 20:21:25 +0200

    change default to HE IL

--- apps/client/src/libs/lingui.ts
+++ apps/client/src/libs/lingui.ts
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 
 import { dayjsLocales } from "./dayjs";
 
-export const defaultLocale = "en-US";
+export const defaultLocale = "he-IL";
 
 export async function dynamicActivate(locale: string) {
   try {
